fde4e6e203f5bcf4e61c783c4e8abe326d9c20ad
Ivan Kmotorka <xkmotorka@ivan.gmail.com>
2017-01-22 14:42:28 +0100

    final static todos

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.get('/', function(req, res){
     res.send('TODO API Root');
 });
-// GET /todos
+// GET /todos?key=value&q=word
 app.get('/todos', function(req, res){
     var queryParams = req.query;
     var filteredTodos = todos;
@@ -39,6 +39,11 @@ app.get('/todos', function(req, res){
         filteredTodos = _.where(filteredTodos, {completed: true});
     }else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
         filteredTodos = _.where(filteredTodos, {completed: false});
+    }
+    if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0){
+        filteredTodos = _.filter(filteredTodos, function(todo){
+            return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) >= 0;
+        });
     }
     
     res.json(filteredTodos);
