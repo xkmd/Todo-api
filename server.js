c50741761f82e09a115c5ffadf7ee04d66933577
Ivan Kmotorka <xkmotorka@ivan.gmail.com>
2017-01-22 11:27:50 +0100

    add delete todos route

--- server.js
+++ server.js
@@ -50,7 +50,9 @@ app.get('/todos/:id', function(req, res){
 // POST /todos/ 
 app.post('/todos', function(req, res){
     var body = _.pick(req.body, 'description', 'completed');
+    
     body.description = body.description.trim();
+    
     if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.length === 0){
         return res.status(400).send();
     }
@@ -60,6 +62,18 @@ app.post('/todos', function(req, res){
     res.json(body);
 });
 
+// DELETE /todos/:id
+app.delete('/todos/:id', function(req,res){
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos,{id: todoId});
+    if(!matchedTodo)
+        res.status(404).json({"error":"no todo find with that id"});
+    else{
+        todos = _.without(todos, matchedTodo);
+        res.json(matchedTodo);
+    }
+    
+});
 
 app.listen(PORT, function(){
     console.log('Express listening on port ' + PORT + '!');
