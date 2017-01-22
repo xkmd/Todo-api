a26c6ebe1156bf072cdc479350cb0ab576a9cb66
Ivan Kmotorka <xkmotorka@ivan.gmail.com>
2017-01-22 12:00:58 +0100

    add PUT todos

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+// 404 - not found
+// 400 - bad syntax
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+
 //var todos = [{
 //    id: 1,
 //    description: 'Meet mom for lunch',
@@ -73,6 +77,34 @@ app.delete('/todos/:id', function(req,res){
         res.json(matchedTodo);
     }
     
+});
+
+// PUT /todos/:id
+
+app.put('/todos/:id', function(req, res){
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos,{id: todoId});
+    
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+    
+    if(!matchedTodo)
+        return res.status(404).send();
+    
+    
+    if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+        validAttributes.completed = body.completed;
+    }else if(body.hasOwnProperty('completed')){
+        return res.status(400).send();
+    }
+    if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+        validAttributes.description = body.description.trim();
+    }else if(body.hasOwnProperty('description')){
+        return res.status(400).send();
+    }
+    
+    _.extend(matchedTodo, validAttributes);
+    res.json(matchedTodo);
 });
 
 app.listen(PORT, function(){
