aeb2d0424e17d0149a5a46d6e4a83d5822a55b75
Ivan Kmotorka <xkmotorka@ivan.gmail.com>
2017-01-22 20:29:02 +0100

    add todo model and POST /todos

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
-
+var db = require('./db.js');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -65,20 +65,42 @@ app.get('/todos/:id', function(req, res){
     //res.send('Asking for todo with id of ' + req.params.id);
 });
 
-// POST /todos/ 
+// with db
 app.post('/todos', function(req, res){
     var body = _.pick(req.body, 'description', 'completed');
     
-    body.description = body.description.trim();
-    
-    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.length === 0){
-        return res.status(400).send();
-    }
-    body.id = todoNextId++;
-    todos.push(body);
-    //console.log('description ' + body.description);
-    res.json(body);
+    db.todo.create(body).then(function(todo){
+        res.json(todo.toJSON());
+    }, function (e){
+        res.status(400).json(e);
+    })
+//    
+//    
+//    body.description = body.description.trim();
+//    
+//    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.length === 0){
+//        return res.status(400).send();
+//    }
+//    body.id = todoNextId++;
+//    todos.push(body);
+//    //console.log('description ' + body.description);
+//    res.json(body);
 });
+
+// POST /todos/     without db
+//app.post('/todos', function(req, res){
+//    var body = _.pick(req.body, 'description', 'completed');
+//    
+//    body.description = body.description.trim();
+//    
+//    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.length === 0){
+//        return res.status(400).send();
+//    }
+//    body.id = todoNextId++;
+//    todos.push(body);
+//    //console.log('description ' + body.description);
+//    res.json(body);
+//});
 
 // DELETE /todos/:id
 
@@ -121,7 +143,14 @@ app.put('/todos/:id', function(req, res){
     _.extend(matchedTodo, validAttributes);
     res.json(matchedTodo);
 });
-
-app.listen(PORT, function(){
-    console.log('Express listening on port ' + PORT + '!');
+//with db
+db.sequelize.sync().then(function(){
+    app.listen(PORT, function(){
+        console.log('Express listening on port ' + PORT + '!');
+    });
 });
+
+// without db
+//app.listen(PORT, function(){
+//    console.log('Express listening on port ' + PORT + '!');
+//});
