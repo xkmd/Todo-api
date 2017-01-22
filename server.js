bd16e560119e322eea9674ed141616846c8e01ed
Ivan Kmotorka <xkmotorka@ivan.gmail.com>
2017-01-23 00:39:58 +0100

    Update PUT /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -155,28 +155,53 @@ app.delete('/todos/:id', function(req,res){
 
 app.put('/todos/:id', function(req, res){
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos,{id: todoId});
-    
     var body = _.pick(req.body, 'description', 'completed');
-    var validAttributes = {};
+    var attributes = {};
+    //with db
     
-    if(!matchedTodo)
-        return res.status(404).send();
-    
-    
-    if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
-        validAttributes.completed = body.completed;
-    }else if(body.hasOwnProperty('completed')){
-        return res.status(400).send();
-    }
-    if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
-        validAttributes.description = body.description.trim();
-    }else if(body.hasOwnProperty('description')){
-        return res.status(400).send();
+    if(body.hasOwnProperty('completed')){
+        attributes.completed = body.completed;
     }
     
-    _.extend(matchedTodo, validAttributes);
-    res.json(matchedTodo);
+    if(body.hasOwnProperty('description')){
+        attributes.description = body.description;
+    }
+    console.log(attributes.description);
+    db.todo.findById(todoId).then(function(todo){
+        if(todo){
+            todo.update(attributes).then(function(todo){
+                console.log(todo.description);
+                res.json(todo.toJSON());
+            },function(e){
+                res.status(400).json(e);
+            });
+        }else{
+            res.status(404).send();
+        }
+    },function(){
+        res.status(500).send();
+    });
+    
+    //without db
+//    var matchedTodo = _.findWhere(todos,{id: todoId});
+//    
+//    if(!matchedTodo)
+//        return res.status(404).send();
+//    
+//    
+//    if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+//        validAttributes.completed = body.completed;
+//    }else if(body.hasOwnProperty('completed')){
+//        return res.status(400).send();
+//    }
+//    if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+//        validAttributes.description = body.description.trim();
+//    }else if(body.hasOwnProperty('description')){
+//        return res.status(400).send();
+//    }
+//    
+//    _.extend(matchedTodo, validAttributes);
+//    res.json(matchedTodo);
 });
 //with db
 db.sequelize.sync().then(function(){
