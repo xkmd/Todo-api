b45ce36f491471deed5cf2cd939311a772d3aca4
Ivan Kmotorka <xkmotorka@ivan.gmail.com>
2017-01-22 13:28:14 +0100

    add variable-playground area

---
+++ variable-playground.js
@@ -0,0 +1,17 @@
+var grades = [22,33];
+
+function addGrades(gradesArr){
+    gradesArr.push(55);
+    debugger;
+}
+
+addGrades(grades);
+console.log(grades);
+
+//debugger
+//node debug .....
+//cont
+//repl
+//exit
+//kill
+//quit
